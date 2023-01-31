bc52e8e8fb4d337e531507b450fa5f9ce83cef86
Gideon Addo <gideonaddoofori@gmail.com>
2023-01-31 18:48:52 +0000

    Add accessibility to list items

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -7,16 +7,19 @@ const Question = ({ questionsData }) => {
   return (
     <div className="animate-fadeIn only:w-full flex flex-col gap-3 pb-5 border-b border-blue100">
       <p className="text-blue300 font-karla font-bold text-base sm:text-lg">{removeUnicode(questionsData.question)}</p>
-      <div className="flex gap-3 w-full flex-wrap">
+      <ul role="listbox" className="flex gap-3 w-full flex-wrap">
         {questionsData.answers.map((answer) => (
           <button
             key={answer.id}
+            type='button'
+            role="option"
+            aria-selected={answer.selected ? "true" : "false"}
             onClick={() => { selectAnswer(answer.id, questionsData.id) }}
             className={`${handleClassNames(answer, questionsData, state.answersChecked)} ${state.answersChecked && "pointer-events-none"} border-blue200 rounded-xl text-blue300 font-inter text-sm font-medium min-w-[5rem] px-4 py-1 bg-blue100 transition-colors text-justify sm:text-base`}>
             {removeUnicode(answer.item)}
           </button>
         ))}
-      </div>
+      </ul>
     </div>
   )
 }
