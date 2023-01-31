a3610733fb093d24115e70ff39d497dd038f6e66
Gideon Addo <gideonaddoofori@gmail.com>
2023-01-31 18:50:56 +0000

    Add accessibility to buttons

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -28,15 +28,15 @@ const Questions = () => {
 
       {!state.answersChecked
         ? (
-          <button onClick={checkAnswers} className={`${state.loading && "pointer-events-none"} animate-fadeIn self-center justify-self-end bg-blue200 text-center rounded-xl text-white100 w-full max-w-[10rem] py-4 font-inter text-sm font-semibold tracking-wide mt-5 transition-transform active:scale-95 sm:text-base sm:max-w-[12rem]`}>Check answers</button>
+          <button type='button' aria-label='Check answers' onClick={checkAnswers} className={`${state.loading && "pointer-events-none"} animate-fadeIn self-center justify-self-end bg-blue200 text-center rounded-xl text-white100 w-full max-w-[10rem] py-4 font-inter text-sm font-semibold tracking-wide mt-5 transition-transform active:scale-95 sm:text-base sm:max-w-[12rem]`}>Check answers</button>
         ) : (
           <div className='animate-fadeIn justify-self-end w-full flex flex-col gap-6 justify-center items-center mt-5'>
             <p className="font-inter text-blue300 font-bold text-sm">
               You scored {`${state.score}/${state.amount}`} correct answers
             </p>
             <div className="flex items-center justify-center flex-wrap gap-5 w-full">
-              <button onClick={playAgain} className="self-center basis-[8.5rem] bg-blue200 text-center rounded-xl text-white100 w-full py-3 font-inter text-sm font-semibold tracking-wide transition-transform active:scale-95 sm:text-base">Play again</button>
-              <button onClick={restartQuiz} className="self-center basis-[8.5rem] bg-blue200 text-center rounded-xl text-white100 w-full py-3 font-inter text-sm font-semibold tracking-wide transition-transform active:scale-95 sm:text-base">Restart quiz</button>
+              <button type='button' aria-label='Play again' onClick={playAgain} className="self-center basis-[8.5rem] bg-blue200 text-center rounded-xl text-white100 w-full py-3 font-inter text-sm font-semibold tracking-wide transition-transform active:scale-95 sm:text-base">Play again</button>
+              <button type='button' aria-label='Restart quiz' onClick={restartQuiz} className="self-center basis-[8.5rem] bg-blue200 text-center rounded-xl text-white100 w-full py-3 font-inter text-sm font-semibold tracking-wide transition-transform active:scale-95 sm:text-base">Restart quiz</button>
             </div>
           </div>
         )
